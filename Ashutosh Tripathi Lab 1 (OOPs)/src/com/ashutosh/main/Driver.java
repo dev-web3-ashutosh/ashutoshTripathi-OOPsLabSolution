ef28f29bb28d38868507122a33d51594eb9ccec1
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-19 20:37:44 +0530

    create Employee object

--- Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/main/Driver.java
+++ Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/main/Driver.java
@@ -1,8 +1,12 @@
 package com.ashutosh.main;
 
+import com.ashutosh.model.Employee;
+
 public class Driver {
 
 	public static void main(String[] args) {
+		
+		Employee obj = new Employee("ashutosh", "Tripathi");
 		
 		
 
