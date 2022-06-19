9da9e0a2d03a17e54f53dfa3e2c394e126ced214
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-19 22:04:58 +0530

    define method generate password

--- Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/service/CredentialService.java
+++ Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/service/CredentialService.java
@@ -1,12 +1,27 @@
 package com.ashutosh.service;
 
+import java.util.Random;
+
 import com.ashutosh.model.Employee;
 
 public class CredentialService {
 	
 	public char[] generatePassword() {
 		
-		char[] pwd;
+		String capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String smallLetters = capitalLetters.toLowerCase();
+		String numbers = "0123456789";
+		String specialCharacters = "!@#$%^&*_=+-/.?<>()";
+		
+		String values = capitalLetters + smallLetters + numbers + specialCharacters;
+		
+		Random rand = new Random();
+		
+		char[] pwd = new char[8];
+		
+		for(int i=0; i<8; i++) {
+			pwd[i] = values.charAt(rand.nextInt(values.length()));
+		}
 		
 		return pwd;
 	}
