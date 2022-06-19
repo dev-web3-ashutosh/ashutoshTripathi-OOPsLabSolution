2c7b456162c7b14f57ecdbfd2fab46af5955d31e
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-19 20:42:18 +0530

    display options and take user input

--- Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/main/Driver.java
+++ Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/main/Driver.java
@@ -1,12 +1,25 @@
 package com.ashutosh.main;
 
+import java.util.Scanner;
+
 import com.ashutosh.model.Employee;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		
 		Employee obj = new Employee("ashutosh", "Tripathi");
+		
+		System.out.println("Please enter the department from the following");
+		System.out.println("1. Technical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+		
+		choice = sc.nextInt();
 		
 		
 
