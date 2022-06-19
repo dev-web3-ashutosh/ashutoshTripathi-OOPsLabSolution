7db35214b32f73640b6396fc05d9411162be6be1
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-19 21:35:37 +0530

    wrriting class credential service

--- Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/service/CredentialService.java
+++ Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/service/CredentialService.java
@@ -1,5 +1,28 @@
 package com.ashutosh.service;
 
+import com.ashutosh.model.Employee;
+
 public class CredentialService {
+	
+	public char[] generatePassword() {
+		
+		char[] pwd;
+		
+		return pwd;
+	}
+	
+	public String generateEmailAddress(String firstName, String lastName, String department) {
+		
+		String company = "google";
+		String email = "";
+		email = firstName + lastName + "@" + department + "." + company + ".com";
+		return email;
+	}
+	
+	public void showCredentials(Employee empObj, String email, char[] password) {
+		
+		System.out.println("Dear " + empObj.getFirstName() + "your generated credentials are as follows");
+		System.out.println("Email --> " + this.generateEmailAddress();
+	}
 
 }
