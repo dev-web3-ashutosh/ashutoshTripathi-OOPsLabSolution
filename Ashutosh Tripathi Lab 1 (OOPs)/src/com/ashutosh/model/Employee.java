1dd3e53369e807e2c0d0a74faf54ee5a3693f863
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-19 20:27:00 +0530

    generetae constructor using ide

--- Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/model/Employee.java
+++ Ashutosh Tripathi Lab 1 (OOPs)/src/com/ashutosh/model/Employee.java
@@ -3,6 +3,11 @@ package com.ashutosh.model;
 public class Employee {
 	
 	private String firstName, lastName;
+
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 	
 	
 
